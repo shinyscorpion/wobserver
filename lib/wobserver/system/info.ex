76b684cff40bdffec68736d12ebbc4d28c25792e
Ian Luites <IanLuites@users.noreply.github.com>
2017-02-06 12:52:33 +0000

    Add System.Info.

---
+++ lib/wobserver/system/info.ex
@@ -0,0 +1,83 @@
+defmodule Wobserver.System.Info do
+  @moduledoc ~S"""
+  Handles general System info like architecture and cpu.
+  """
+
+  alias Wobserver.System.Info
+
+  @typedoc ~S"""
+  Architecture information.
+  """
+  @type t :: %__MODULE__{
+    otp_release: String.t,
+    elixir_version: String.t,
+    erts_version: String.t,
+    system_architecture: String.t,
+    kernel_poll: boolean,
+    smp_support: boolean,
+    threads: boolean,
+    thread_pool_size: integer,
+    wordsize_internal: integer,
+    wordsize_external: integer,
+  }
+
+  defstruct [
+    otp_release: "",
+    elixir_version: "",
+    erts_version: "",
+    system_architecture: "",
+    kernel_poll: false,
+    smp_support: false,
+    threads: false,
+    thread_pool_size: 0,
+    wordsize_internal: 0,
+    wordsize_external: 0,
+  ]
+
+  @doc ~S"""
+  Returns architecture information.
+  """
+  @spec architecture :: Info.t
+  def architecture do
+    %Info{
+      otp_release: to_string(:erlang.system_info(:otp_release)),
+      elixir_version: System.version,
+      erts_version: to_string(:erlang.system_info(:version)),
+      system_architecture: to_string(:erlang.system_info(:system_architecture)),
+      kernel_poll: :erlang.system_info(:kernel_poll),
+      smp_support: :erlang.system_info(:smp_support),
+      threads: :erlang.system_info(:threads),
+      thread_pool_size: :erlang.system_info(:thread_pool_size),
+      wordsize_internal: :erlang.system_info({:wordsize, :internal}),
+      wordsize_external: :erlang.system_info({:wordsize, :internal}),
+
+    }
+  end
+
+  @doc ~S"""
+  Returns cpu information.
+  """
+  def cpu do
+    schedulers = :erlang.system_info(:logical_processors)
+    schedulers_available =
+      case :erlang.system_info(:multi_scheduling) do
+        :enabled -> schedulers
+        _ -> 1
+      end
+
+    %{
+      logical_processors:
+        :erlang.system_info(:logical_processors),
+      logical_processors_online:
+        :erlang.system_info(:logical_processors_online),
+      logical_processors_available:
+        :erlang.system_info(:logical_processors_available),
+      schedulers:
+        :erlang.system_info(:schedulers),
+      schedulers_online:
+        schedulers,
+      schedulers_available:
+        schedulers_available,
+    }
+  end
+end
---
+++ test/wobserver/system/info_test.exs
@@ -0,0 +1,53 @@
+defmodule Wobserver.System.InfoTest do
+  use ExUnit.Case
+
+  describe "architecture" do
+    test "returns info struct" do
+      assert %Wobserver.System.Info{} = Wobserver.System.Info.architecture
+    end
+
+    test "returns values" do
+      %Wobserver.System.Info{
+        otp_release: otp_release,
+        elixir_version: elixir_version,
+        erts_version: erts_version,
+        system_architecture: system_architecture,
+        kernel_poll: kernel_poll,
+        smp_support: smp_support,
+        threads: threads,
+        thread_pool_size: thread_pool_size,
+        wordsize_internal: wordsize_internal,
+        wordsize_external: wordsize_external,
+      } = Wobserver.System.Info.architecture
+
+      assert is_binary(otp_release)
+      assert is_binary(elixir_version)
+      assert is_binary(erts_version)
+      assert is_binary(system_architecture)
+      assert is_boolean(kernel_poll)
+      assert is_boolean(smp_support)
+      assert is_boolean(threads)
+      assert is_integer(thread_pool_size)
+      assert is_integer(wordsize_internal)
+      assert is_integer(wordsize_external)
+    end
+  end
+
+  test "cpu returns values" do
+    %{
+      logical_processors: logical_processors,
+      logical_processors_online: logical_processors_online,
+      logical_processors_available: logical_processors_available,
+      schedulers: schedulers,
+      schedulers_online: schedulers_online,
+      schedulers_available: schedulers_available,
+    } = Wobserver.System.Info.cpu
+
+    assert is_integer(logical_processors)
+    assert is_integer(logical_processors_online)
+    assert is_integer(logical_processors_available) || logical_processors_available == :unknown
+    assert is_integer(schedulers)
+    assert is_integer(schedulers_online)
+    assert is_integer(schedulers_available)
+  end
+end
