92c362be59a2e8ceab70c7dc04757e816875a05a
Ian Luites <IanLuites@users.noreply.github.com>
2017-02-08 16:28:15 +0000

    Add Metrics.Formatter

---
+++ lib/wobserver/util/metrics/formatter.ex
@@ -0,0 +1,149 @@
+defmodule Wobserver.Util.Metrics.Formatter do
+  @moduledoc ~S"""
+  Formatter.
+
+  TODO: Needs config.
+  """
+
+  alias Wobserver.Util.Node.Discovery
+
+  @callback format_data(
+    name :: String.t,
+    data :: [{integer | float, keyword}],
+    type :: :atom,
+    help :: String.t
+  ) :: String.t
+
+  @callback combine_metrics(
+    metrics :: list[String.t]
+  ) :: String.t
+
+  @callback merge_metrics(
+    metrics :: list[String.t]
+  ) :: String.t
+
+  @spec format(
+    data :: any,
+    label :: String.t,
+    type :: :gauge | :counter | nil,
+    help :: String.t | nil,
+    formatter :: atom | nil
+  ) :: String.t | :error
+  def format(data, label, type \\ nil, help \\ nil, formatter \\ nil)
+
+  def format(data, label, type, help, nil) do
+    format(data, label, type, help, Application.get_env(
+      :wobserver,
+      :metric_format,
+      Wobserver.Util.Metrics.Prometheus
+    ))
+  end
+
+  def format(data, label, type, help, formatter) when is_binary(formatter) do
+    {new_formatter, []} = Code.eval_string formatter
+
+    format(data, label, type, help, new_formatter)
+  end
+
+  def format(data, label, type, help, formatter) do
+    cond do
+      is_map(data) ->
+        data
+        |> map_to_data()
+        |> format(label, type, help, formatter)
+      Keyword.keyword?(data) ->
+        data
+        |> list_to_data()
+        |> format(label, type, help, formatter)
+      is_binary(data) ->
+        try do
+          {new_data, []} = Code.eval_string data
+          format(new_data, label, type, help, formatter)
+        catch
+          :error, _ -> :error
+        end
+      is_function(data) ->
+        data.()
+        |> format(label, type, help, formatter)
+      is_list(data) ->
+        data =
+          data
+          |> Enum.map(fn {value, labels} ->
+              {value, Keyword.merge([node: Discovery.local.name], labels)}
+            end)
+
+        formatter.format_data(label, data, type, help)
+      true ->
+        :error
+    end
+  end
+
+  @spec format_all(data :: list, formatter :: atom) :: String.t | :error
+  def format_all(data, formatter \\ nil)
+
+  def format_all(data, nil) do
+    format_all(data, Application.get_env(
+      :wobserver,
+      :metric_format,
+      Wobserver.Util.Metrics.Prometheus
+    ))
+  end
+
+  def format_all(data, formatter) do
+    formatted =
+      data
+      |> Enum.map(&helper(&1, formatter))
+
+    case Enum.member?(formatted, :error) do
+      true -> :error
+      _ -> formatted |> formatter.combine_metrics
+    end
+  end
+
+  @spec merge_metrics(metrics :: list(String.t), formatter :: atom)
+   :: String.t | :error
+  def merge_metrics(metrics, formatter \\ nil)
+
+  def merge_metrics(metrics, nil) do
+    merge_metrics(metrics,
+      Application.get_env(
+        :wobserver,
+        :metric_format,
+        Wobserver.Util.Metrics.Prometheus
+      )
+    )
+  end
+
+  def merge_metrics(metrics, formatter) do
+    case Enum.member?(metrics, :error) do
+      true -> :error
+      false -> formatter.merge_metrics(metrics)
+    end
+  end
+
+  # Helpers
+
+  defp helper({key, {data, type, help}}, formatter) do
+    format(data, Atom.to_string(key), type, help, formatter)
+  end
+
+  defp helper({key, {data, type}}, formatter) do
+    format(data, Atom.to_string(key), type, nil, formatter)
+  end
+
+  defp helper({key, data}, formatter) do
+    format(data, Atom.to_string(key), nil, nil, formatter)
+  end
+
+  defp map_to_data(map) do
+    map
+    |> Map.to_list
+    |> Enum.filter(fn {a, _} -> a != :__struct__ && a != :total  end)
+    |> list_to_data()
+  end
+
+  defp list_to_data(list) do
+    list
+    |> Enum.map(fn {key, value} -> {value, [type: key]} end)
+  end
+end
---
+++ test/wobserver/util/metrics/formatter_test.exs
@@ -0,0 +1,137 @@
+defmodule Wobserver.Util.Metrics.FormatterTest do
+  use ExUnit.Case, async: false
+
+  alias Wobserver.Util.Metrics.{
+    Formatter,
+    Prometheus,
+  }
+
+  def example_function do
+    [point: 5]
+  end
+
+  def local_ip do
+    with  {:ok, ips} <- :inet.getif(),
+          {ip, _, _} <- List.first(ips),
+          {ip1, ip2, ip3, ip4} <- ip,
+      do: "#{ip1}.#{ip2}.#{ip3}.#{ip4}"
+  end
+
+  describe "format" do
+    test "returns with valid data" do
+      assert Formatter.format(
+        %{point: 5},
+        "data"
+      ) == "data{node=\"#{local_ip()}\",type=\"point\"} 5\n"
+    end
+
+    test "returns with valid data and type" do
+      assert Formatter.format(
+        %{point: 5},
+        "data",
+        :gauge
+      ) == "# TYPE data gauge\ndata{node=\"#{local_ip()}\",type=\"point\"} 5\n"
+    end
+
+    test "returns with valid data, type, and help" do
+      assert Formatter.format(
+        %{point: 5},
+        "data",
+        :gauge,
+        "help"
+      ) == "# HELP data help\n# TYPE data gauge\ndata{node=\"#{local_ip()}\",type=\"point\"} 5\n"
+    end
+
+    test "returns with valid data as keywords" do
+      assert Formatter.format(
+        [point: 5],
+        "data"
+      ) == "data{node=\"#{local_ip()}\",type=\"point\"} 5\n"
+    end
+
+    test "returns with valid data as data String" do
+      assert Formatter.format(
+        "[point: 5]",
+        "data"
+      ) == "data{node=\"#{local_ip()}\",type=\"point\"} 5\n"
+    end
+
+    test "returns with valid data as anon function" do
+      assert Formatter.format(
+        "fn -> [point: 5] end",
+        "data"
+      ) == "data{node=\"#{local_ip()}\",type=\"point\"} 5\n"
+    end
+
+    test "returns with valid data as function call" do
+      assert Formatter.format(
+        "Wobserver.Util.Metrics.FormatterTest.example_function",
+        "data"
+      ) == "data{node=\"#{local_ip()}\",type=\"point\"} 5\n"
+    end
+
+    test "returns with valid data as function" do
+      assert Formatter.format(
+        "&Wobserver.Util.Metrics.FormatterTest.example_function/0",
+        "data"
+      ) == "data{node=\"#{local_ip()}\",type=\"point\"} 5\n"
+    end
+
+    test "returns with explicit formatter" do
+      assert Formatter.format(
+        [point: 5],
+        "data",
+        nil,
+        nil,
+        Wobserver.Util.Metrics.Prometheus
+      ) == "data{node=\"#{local_ip()}\",type=\"point\"} 5\n"
+    end
+
+    test "returns with explicit formatter as String" do
+      assert Formatter.format(
+        [point: 5],
+        "data",
+        nil,
+        nil,
+        "Wobserver.Util.Metrics.Prometheus"
+      ) == "data{node=\"#{local_ip()}\",type=\"point\"} 5\n"
+    end
+  end
+
+  describe "format_all" do
+    test "returns :error with invalid entry" do
+      assert Formatter.format_all([
+        works: %{value: 8},
+        invalid: "1",
+      ]) == :error
+    end
+
+    test "returns with multiple entries" do
+      assert Formatter.format_all([
+        works: %{value: 8},
+        also_works: %{value: 9},
+      ], Prometheus) == "works{node=\"#{local_ip()}\",type=\"value\"} 8\nalso_works{node=\"#{local_ip()}\",type=\"value\"} 9\n"
+    end
+
+    test "returns with multiple entries and type" do
+      assert Formatter.format_all([
+        works: {
+          %{value: 8},
+          :gauge
+        },
+        also_works: %{value: 9},
+      ], Prometheus) == "# TYPE works gauge\nworks{node=\"#{local_ip()}\",type=\"value\"} 8\nalso_works{node=\"#{local_ip()}\",type=\"value\"} 9\n"
+    end
+
+    test "returns with multiple entries + type & help" do
+      assert Formatter.format_all([
+        works: {
+          %{value: 8},
+          :gauge,
+          "Info"
+        },
+        also_works: %{value: 9},
+      ], Prometheus) == "# HELP works Info\n# TYPE works gauge\nworks{node=\"#{local_ip()}\",type=\"value\"} 8\nalso_works{node=\"#{local_ip()}\",type=\"value\"} 9\n"
+    end
+  end
+end
