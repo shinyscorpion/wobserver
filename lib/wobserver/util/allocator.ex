c68ca720a54f78ab51027cece7d7684c89dea2a3
Ian Luites <IanLuites@users.noreply.github.com>
2017-02-10 22:35:59 +0000

    Add Allocator.

---
+++ lib/wobserver/util/allocator.ex
@@ -0,0 +1,36 @@
+defmodule Wobserver.Allocator do
+  @moduledoc ~S"""
+  Allocator
+
+  TODO:
+    - Needs docs.
+    - Needs cleanup.
+    - Needs tests.
+  """
+
+  @spec list :: list(map)
+  def list do
+    :alloc_util_allocators
+    |> :erlang.system_info
+    |> info()
+  end
+
+  defp info(type) do
+    {:allocator_sizes, type}
+    |> :erlang.system_info
+    |> Enum.map(&sum_data/1)
+    |> Enum.filter(&non_zero?/1)
+  end
+
+  defp non_zero?(%{carrier: c, block: b}), do: c != 0 && b != 0
+
+  defp sum_data({type, data}) do
+    data
+    |> Enum.map(fn {_, _, d} -> Keyword.get(d,:mbcs) end)
+    |> Enum.map(&block_and_carrier_size/1)
+    |> Enum.reduce({0,0}, fn {x, y}, {a, b} -> {a + x, y + b} end)
+    |> (fn {x, y} -> %{type: type, block: x, carrier: y} end).()
+  end
+
+  defp block_and_carrier_size([{_, x, _, _}, {_, y, _, _}]), do: {x, y}
+end
