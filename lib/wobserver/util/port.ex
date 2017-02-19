10cae8ec225c7e227e9da7361c9e6929c1af9131
Ian Luites <IanLuites@users.noreply.github.com>
2017-02-19 20:13:45 +0000

    Fix port undefined issue. (See: #13)

--- lib/wobserver/util/port.ex
+++ lib/wobserver/util/port.ex
@@ -20,22 +20,31 @@ defmodule Wobserver.Port do
   def list do
     :erlang.ports
     |> Enum.map(&info/1)
+    |> Enum.filter(fn
+        :port_not_found -> false
+        _ -> true
+       end)
   end
 
   defp info(port) do
-    data =
+    port_data =
       port
       |> :erlang.port_info
 
-    %{
-      id: Keyword.get(data, :id, -1),
-      port: port,
-      name: to_string(Keyword.get(data, :name, '')),
-      links: Keyword.get(data, :links, []),
-      connected: Keyword.get(data, :connected, nil),
-      input: Keyword.get(data, :input, 0),
-      output: Keyword.get(data, :output, 0),
-      os_pid: Keyword.get(data, :os_pid, nil),
-    }
+    case port_data do
+      :undefined ->
+        :port_not_found
+      data ->
+        %{
+          id: Keyword.get(data, :id, -1),
+          port: port,
+          name: to_string(Keyword.get(data, :name, '')),
+          links: Keyword.get(data, :links, []),
+          connected: Keyword.get(data, :connected, nil),
+          input: Keyword.get(data, :input, 0),
+          output: Keyword.get(data, :output, 0),
+          os_pid: Keyword.get(data, :os_pid, nil),
+        }
+    end
   end
 end
