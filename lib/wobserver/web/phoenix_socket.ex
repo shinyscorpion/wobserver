01ce73428b505a45277c5461a31a41863e5791ec
OvermindDL1 <overminddl1@gmail.com>
2017-02-21 11:08:49 -0700

    Add Phoenix Socket support so no need to replace the Phoenix endpoint with a manually crafted cowboy config

---
+++ lib/wobserver/web/phoenix_socket.ex
@@ -0,0 +1,28 @@
+defmodule Wobserver.Web.PhoenixSocket do
+  @moduledoc """
+  Drop-in Phoenix Socket for easier integration in to Phoenix endpoints
+
+  Example:
+    ```elixir
+    defmodule MyPhoenixServer.Endpoint do
+      socket "/wobserver", Wobserver.Web.PhoenixSocket # The path should be the same as the router path
+
+      ...
+    end
+    ```
+  """
+
+  @doc false
+  def __transports__ do
+    config = [
+      cowboy: Wobserver.Web.Client
+    ]
+    callback_module = Wobserver.Web.PhoenixSocket
+    transport_path = :ws
+    websocket_socket = {transport_path, {callback_module, config}}
+    # Only handling one type, websocket, no longpolling or anything else
+    [
+      websocket_socket
+    ]
+  end
+end
