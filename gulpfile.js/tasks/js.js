7017a5e5f92e23f0feeb09d0aa1e21728e1a96cd
Ian Luites <IanLuites@users.noreply.github.com>
2017-03-17 19:44:28 +0000

    Fix issue #17 and partially fix #25.
    Add external resources into baked file.

--- gulpfile.js/tasks/js.js
+++ gulpfile.js/tasks/js.js
@@ -11,8 +11,9 @@ const babelify   = require('babelify');
 const source     = require('vinyl-source-stream');
 const buffer     = require('vinyl-buffer');
 const uglify     = require('gulp-uglify');
+const concat     = require("gulp-concat");
 
-gulp.task('js', () => {
+gulp.task('create-js', () => {
   return browserify({entries: path.join(config.root.src, config.js.src, 'app.js'), extensions: ['.js'], debug: true})
     .transform(babelify, {
       sourceMapsAbsolute: true,
@@ -26,7 +27,7 @@ gulp.task('js', () => {
     .pipe(gulp.dest(path.join(config.root.dest, config.js.dest)));
 });
 
-gulp.task('js-prod', () => {
+gulp.task('create-js-prod', () => {
   return browserify({entries: path.join(config.root.src, config.js.src, 'app.js'), extensions: ['.js'], debug: false})
     .transform(babelify, {
       sourceMapsAbsolute: false,
@@ -36,5 +37,28 @@ gulp.task('js-prod', () => {
     .pipe(source('app.js'))
     .pipe(buffer())
     .pipe(uglify())
+    .pipe(gulp.dest(path.join(config.root.dest, config.js.dest)));
+});
+
+gulp.task('js', ['create-js'], function() {
+    return gulp.src([
+        path.join(config.root.src, config.js.src, 'external/*'),
+        path.join(config.root.dest, config.js.dest, 'app.js')
+    ])
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(concat('app.js'))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(path.join(config.root.dest, config.js.dest)));
+});
+
+gulp.task('js-prod', ['create-js-prod'], function() {
+    return gulp.src([
+        path.join(config.root.src, config.js.src, 'external/*'),
+        path.join(config.root.dest, config.js.dest, 'app.js')
+    ])
+    .pipe(buffer())
+    .pipe(uglify())
+    .pipe(concat('app.js'))
     .pipe(gulp.dest(path.join(config.root.dest, config.js.dest)));
 });
